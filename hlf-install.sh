54f3a71c0f724fbf8b57bda7a7933132ef0df1b8
SmartInsider <smartyinsider@gmail.com>
2020-05-29 13:03:22 +0300

    first commit

---
+++ hlf-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt-get update --yes && apt-get install --yes build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev libevent-dev wget software-properties-common
+add-apt-repository --yes ppa:bitcoin/bitcoin && apt-get update
+apt-get install --yes libdb4.8-dev libdb4.8++-dev libminiupnpc-dev libzmq3-dev git jq
+
+wget https://github.com/happyleaf-money/happyleaf/releases/download/4.3.1.0/happyleafd && chmod 744 happyleafd
+wget https://github.com/happyleaf-money/happyleaf/releases/download/4.3.1.0/happyleaf-cli && chmod 744 happyleaf-cli
+
+
+rpcuser="happyleaf"
+rpcpassword="oY06p4d$UI"
+
+mnkey=$1
+mnNUMBER=$NUMBER
+./happyleafd --daemon --masternode=1 --masternodeprivkey=${mnkey} --rpcuser=${rpcuser} --rpcpassword=${rpcpassword}
+
+
+
